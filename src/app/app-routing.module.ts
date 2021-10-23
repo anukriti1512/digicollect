025070c6eb3d351a8a364fa3ebd278d31007ea49
Anukriti chourasia <anukriti1512@gmail.com>
2021-10-23 16:21:05 +0530

    routing added , profile tab

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { Content } from '@angular/compiler/src/render3/r3_ast';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/content',
+    pathMatch: 'full',
+  },
+  { path: 'content', component: ContentComponent },
+  {
+    path: '**',
+    component: ContentComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
